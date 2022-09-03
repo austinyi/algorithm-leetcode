1dc475d01848429f72bc13168e4db6a0b2fcce2e
Bongsoo Yi <austinyi@naver.com>
2022-09-02 23:22:27 -0400

    Update 4-Median-of-Two-Sorted-Arrays.py

--- 4-Median-of-Two-Sorted-Arrays.py
+++ 4-Median-of-Two-Sorted-Arrays.py
@@ -1,4 +1,6 @@
 '''
+1. Using Python built-in function
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         mergedNums = nums1
@@ -9,43 +11,57 @@ class Solution:
             return mergedNums[int(len(mergedNums) / 2)]
         else: # even
             return (mergedNums[int(len(mergedNums) / 2)] + mergedNums[int(len(mergedNums) / 2) - 1]) / 2
-
-'''
+            
+      
+      
+2. Merging two sorted arrays from the beginning
 
 class Solution:
-  
-    # Method to find median
-    def findMedianSortedArrays(self, A, B):
-        
-          # Assumption both A and B cannot be empty
-        n = len(A)
-        m = len(B)
-        if (n > m):
-            return self.findMedianSortedArrays(B, A)  # Swapping to make A smaller
-  
-        start = 0
-        end = n
-        realmidinmergedarray = (n + m + 1) // 2
-  
-        while (start <= end):
-            mid = (start + end) // 2
-            leftAsize = mid
-            leftBsize = realmidinmergedarray - mid
-              
-            # checking overflow of indices
-            leftA = A[leftAsize - 1] if (leftAsize > 0) else float('-inf')
-            leftB = B[leftBsize - 1] if (leftBsize > 0) else float('-inf')
-            rightA = A[leftAsize] if (leftAsize < n) else float('inf')
-            rightB = B[leftBsize] if (leftBsize < m) else float('inf')
-  
-            # if correct partition is done
-            if leftA <= rightB and leftB <= rightA:
-                if ((m + n) % 2 == 0):
-                    return (max(leftA, leftB) + min(rightA, rightB)) / 2.0
-                return max(leftA, leftB)
-  
-            elif (leftA > rightB):
-                end = mid - 1
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        #m, n = len(nums1), len(nums2)
+        mid = (len(nums1)+len(nums2)+1) // 2 
+        m = 0
+        n = 0
+        res = []
+        for i in range(mid+1):
+            n1 = nums1[m] if m < len(nums1) else float('inf')
+            n2 = nums2[n] if n < len(nums2) else float('inf')
+            if n1 > n2:
+                res.append(n2)
+                n += 1
             else:
-                start = mid + 1
+                res.append(n1)
+                m += 1
+
+        return res[-2] if (len(nums1)+len(nums2)) % 2 == 1 else (res[-2] + res[-1])/2
+ 
+''' 
+ 
+# 3. Merging two sorted arrays from the beginning without using any additional memory
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # m, n = len(nums1), len(nums2)
+        mid = (len(nums1)+len(nums2)+1) // 2 
+        m = 0
+        n = 0
+        for i in range(mid):
+            n1 = nums1[m] if m < len(nums1) else float('inf')
+            n2 = nums2[n] if n < len(nums2) else float('inf')
+            if n1 > n2:
+                n += 1
+            else:
+                m += 1
+        
+        a = nums1[m-1] if m > 0 else float('-inf')
+        b = nums2[n-1] if n > 0 else float('-inf')
+        
+        if (len(nums1)+len(nums2)) % 2 == 1:
+            return max(a,b)
+        else:
+            c = nums1[m] if m < len(nums1) else float('inf')
+            d = nums2[n] if n < len(nums2) else float('inf')
+            return (max(a,b) + min(c,d))/2
+
+
                 
