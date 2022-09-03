516df13c35d1ba103b136562a5d9711bff0aa917
Bongsoo Yi <austinyi@naver.com>
2022-09-03 16:12:37 -0400

    Update 23-Merge-k-Sorted-Lists.py

--- 23-Merge-k-Sorted-Lists.py
+++ 23-Merge-k-Sorted-Lists.py
@@ -15,15 +15,30 @@ class Solution:
         
         return self.merge(left, right)
     
+    def merge(self, l1, l2): # From Leetcode 21. Merge Two Sorted Lists
+        dummy = tail = ListNode()
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+            
+        tail.next = l1 or l2
+      
+        return dummy.next
+    '''
     def merge(self, l1, l2):
         if not l1 or not l2:
             return l1 or l2
-        '''
-        if not l1:
-            return l2
-        elif not l2:
-            return l1
-        '''
+        
+        #if not l1:
+        #    return l2
+        #elif not l2:
+        #    return l1
 
         if l1.val < l2.val:
             l1.next = self.merge(l1.next, l2)
@@ -31,3 +46,5 @@ class Solution:
         else:
             l2.next = self.merge(l2.next, l1)
             return l2
+   '''     
+        
