e32fcb6669e84443df1f20c087da582460af1b1a
Bongsoo Yi <austinyi@naver.com>
2022-09-05 23:23:08 -0400

    Update 733-Flood-Fill.py

--- 733-Flood-Fill.py
+++ 733-Flood-Fill.py
@@ -2,18 +2,21 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
                     
         m, n = len(image), len(image[0])
-        col = image[sr][sc]
-
-        if col == color:
-            return image
+        cri = image[sr][sc]
         
-        def dfs(r,c):
-            if image[r][c] == col:
-                image[r][c] = color
-                if r > 0: dfs(r-1,c)
-                if r < m-1: dfs(r+1,c)
-                if c > 0: dfs(r,c-1)
-                if c < n-1: dfs(r,c+1)
+        if cri == color:
+            return image
 
+        def dfs(r, c):
+            if image[r][c] == cri:
+                image[r][c] = color
+                if r > 0: dfs(r-1, c)
+                if r < m - 1: dfs(r+1, c)
+                if c > 0: dfs(r, c-1)
+                if c < n - 1: dfs(r, c+1)
+        
         dfs(sr, sc)
+        
         return image
+                
+            
