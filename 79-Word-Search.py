fcd3981df46ccd63ff23e95b23d82aa043c2def8
Bongsoo Yi <austinyi@naver.com>
2022-10-15 16:25:47 -0400

    Update 79-Word-Search.py

--- 79-Word-Search.py
+++ 79-Word-Search.py
@@ -1,25 +1,22 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
-        cur = set()
-    
-        def dfs(r, c, i):
-            if i == len(word):
+        path = set()
+
+        def dfs(r, c, cur):
+            if cur == len(word):
                 return True
             if r < 0 or c < 0 or r >= m or c >= n:
                 return False
-            if board[r][c] != word[i] or (r,c) in cur:
+            if board[r][c] != word[cur] or (r, c) in path:
                 return False
-            
-            cur.add((r,c))
-            ans = dfs(r+1, c, i+1) or dfs(r-1, c, i+1) or dfs(r, c+1, i+1) or dfs(r, c-1, i+1)
-            cur.remove((r,c))
-            
-            return ans
-        
-        for i in range(m):
-            for j in range(n):
-                if dfs(i, j, 0):
+            path.add((r, c))
+            res = dfs(r+1, c, cur+1) or dfs(r, c+1, cur+1) or dfs(r-1, c, cur+1) or dfs(r, c-1, cur+1)
+            path.remove((r, c))
+            return res
+
+        for r in range(m):
+            for c in range(n):
+                if dfs(r, c, 0):
                     return True
-                
         return False
