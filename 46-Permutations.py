1fe307b6389db7b8e8ae9ef45ce9aaaff49f2830
Bongsoo Yi <austinyi@naver.com>
2022-10-15 15:46:24 -0400

    Create 46-Permutations.py

---
+++ 46-Permutations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        def dfs(nums, cur):
+            if len(nums) == 1:
+                ans.append(cur + [nums[0]])
+            else:
+                for i in range(len(nums)):
+                    dfs(nums[:i] + nums[i+1:], cur + [nums[i]])
+
+        dfs(nums, [])
+        return ans
