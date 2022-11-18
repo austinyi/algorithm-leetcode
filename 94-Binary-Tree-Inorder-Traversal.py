78fae1edf1cd61762765e6d9d2e10e0e3f659803
Bongsoo Yi <austinyi@naver.com>
2022-11-18 02:26:18 -0500

    Update 94-Binary-Tree-Inorder-Traversal.py

--- 94-Binary-Tree-Inorder-Traversal.py
+++ 94-Binary-Tree-Inorder-Traversal.py
@@ -9,3 +9,16 @@ class Solution:
         if not root:
             return []
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+
+    
+    def inorderTraversal(self, root):
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
