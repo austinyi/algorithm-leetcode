d16f0038ec3e51b0383b2b43e098abf5584c38f2
Bongsoo Yi <austinyi@naver.com>
2022-11-24 23:59:31 -0500

    Update 772-Basic-Calculator-III.py

--- 772-Basic-Calculator-III.py
+++ 772-Basic-Calculator-III.py
@@ -1,1 +1,44 @@
+class Solution:
+    """
+    @param s: the expression string
+    @return: the answer
+    """
+    def calculate(self, s: str) -> int:
+        num = 0
+        op = "+"
+        stack = []
 
+        def helper(num, op):
+            if op == "+":
+                stack.append(num)
+            elif op == "-":
+                stack.append(-num)
+            elif op == "*":
+                stack.append(num * stack.pop())
+            elif op == "/":
+                stack.append(int(stack.pop() / num))
+            
+        for i in range(len(s)):
+            if s[i].isdigit():
+                num = num * 10 + int(s[i])
+            elif s[i] == "(":
+                stack.append(op)
+                op = "+"
+                num = 0
+            elif s[i] == ")":
+                helper(num, op)
+                num = 0
+                while isinstance(stack[-1], int):
+                    num += stack.pop()
+                helper(num, stack.pop())
+                num = 0
+                op = s[i]
+            elif s[i] in ["+", "-", "*", "/"]:
+                helper(num, op)
+                num = 0
+                op = s[i]
+
+        helper(num, op)
+        
+        return sum(stack)
+            
