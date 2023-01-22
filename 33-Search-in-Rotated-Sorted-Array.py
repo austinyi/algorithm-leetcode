575479dac3324a85ec615fa89505572e9a5c9ede
Bongsoo Yi <austinyi@naver.com>
2023-01-22 02:11:38 -0500

    Update 33-Search-in-Rotated-Sorted-Array.py

--- 33-Search-in-Rotated-Sorted-Array.py
+++ 33-Search-in-Rotated-Sorted-Array.py
@@ -25,3 +25,74 @@ class Solution:
                         r = m - 1     
         return -1
     
+
+    
+    '''
+    class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        if nums[l] < nums[r]:
+            l = 0
+            r = len(nums) - 1
+            while l < r:
+                m = (l+r) // 2
+                if nums[m] == target:
+                    return m
+                elif nums[m] > target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            if nums[l] == target:
+                return l
+            else:
+                return -1
+        elif len(nums) == 1:
+            if nums[0] == target:
+                return 0
+            else:
+                return -1
+        else:
+            while l < r:
+                m = (l+r) // 2
+                if nums[m] > nums[l]:
+                    l = m 
+                else:
+                    r = m
+            pivot = l+1
+
+        if target >= nums[0]:
+            l = 0
+            r = pivot - 1
+            while l < r:
+                m = (l+r) // 2
+                if nums[m] == target:
+                    return m
+                elif nums[m] > target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            if nums[l] == target:
+                return l
+            else:
+                return -1
+        else:
+            l = pivot
+            r = len(nums) - 1
+            while l < r:
+                m = (l+r) // 2
+                if nums[m] == target:
+                    return m
+                elif nums[m] > target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            if nums[l] == target:
+                return l
+            else:
+                return -1
+        
+
+
+
+    '''
