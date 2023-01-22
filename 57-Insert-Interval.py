3c5ed0590c44d65f49828e6f36f0082035539f20
Bongsoo Yi <austinyi@naver.com>
2023-01-22 15:09:14 -0500

    Update 57-Insert-Interval.py

--- 57-Insert-Interval.py
+++ 57-Insert-Interval.py
@@ -1,55 +1,29 @@
-'''
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        new = []
-        n = len(intervals)
+        ans = []
+
+        if intervals == []:
+            return [newInterval]
+        elif intervals[0][0] > newInterval[1]:
+            ans.append(newInterval)
+            ans += intervals
+            return ans
+        elif intervals[-1][1] < newInterval[0]:
+            return intervals + [newInterval]
+
         i = 0
-        while i < n and intervals[i][1] < newInterval[0]:
-            new.append(intervals[i])
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            ans.append(intervals[i])
             i += 1
 
-        if i == n:
-            new.append(newInterval)
-            return new
-        #if intervals[i][0] >= newInterval[0]:
-        j = i
-        while j < n and intervals[j][0] <= newInterval[1]:
-            j+=1
-        if j == i:
-            new.append([min(newInterval[0],intervals[i][0]), newInterval[1]])
-        else:
-            new.append([min(newInterval[0],intervals[i][0]), max(newInterval[1],intervals[j-1][1])])
-        
-        #if j<n:
-        for k in range(j,n):
-            print(intervals[k:])
-            new.append(intervals[k:][0])
-        
-        return new
- '''
- 
- class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        i = -1
-        sig = 1
-        if intervals == []:
-            res.append(newInterval)
-            return res
-        
-        for i, (a, b) in enumerate(intervals):
-            if b < newInterval[0]:
-                res.append([a,b])
-            elif a > newInterval[1]:
-                res.append(newInterval)
-                sig = 0
-                i -= 1
-                break
-            else:
-                newInterval[0] = min(a, newInterval[0])
-                newInterval[1] = max(b, newInterval[1])
-        if sig != 0:
-            res.append(newInterval)
+        l = min(intervals[i][0], newInterval[0])
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            i += 1
 
-        return res + intervals[i+1:]
-        
+        r = max(intervals[i-1][1], newInterval[1])
+        ans.append([l,r])
+        while i < len(intervals):
+            ans.append(intervals[i])
+            i += 1
+
+        return ans
