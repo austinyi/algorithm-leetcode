9e09040492be837502d0b430a735377659981e87
Bongsoo Yi <austinyi@naver.com>
2023-01-22 16:09:31 -0500

    Update 46-Permutations.py

--- 46-Permutations.py
+++ 46-Permutations.py
@@ -1,6 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
+        if len(nums) == 1:
+            return [nums]
+        for i in range(len(nums)):
+            for per in self.permute(nums[:i] + nums[i+1:]):
+                ans.append(per + [nums[i]])
+        return ans
+
+    
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
 
         def dfs(nums, cur):
             if len(nums) == 1:
@@ -11,3 +22,6 @@ class Solution:
 
         dfs(nums, [])
         return ans
+
+    
+
