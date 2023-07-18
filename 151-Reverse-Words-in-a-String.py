70cb725180050c95eec8341bafbfd9e65ba050ef
Bongsoo Yi <austinyi@naver.com>
2023-07-17 22:39:26 -0500

    Update 151-Reverse-Words-in-a-String.py

--- 151-Reverse-Words-in-a-String.py
+++ 151-Reverse-Words-in-a-String.py
@@ -1,3 +1,24 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join(reversed(s.split()))
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(s.split()[::-1])
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        res = []
+        cur = ""
+        for c in s:
+            if c == " ":
+                if cur == "":
+                    continue
+                else:
+                    res.append(cur)
+                    cur = ""
+            else:
+                cur += c
+        if cur != "":
+            res.append(cur)
+        return ' '.join(res[::-1])
