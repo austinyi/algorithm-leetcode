c3aff80b78b11a8beed94a6090d8bc36960e5a63
Bongsoo Yi <austinyi@naver.com>
2023-07-22 10:01:55 -0500

    Update 392-Is-Subsequence.py

--- 392-Is-Subsequence.py
+++ 392-Is-Subsequence.py
@@ -14,8 +14,27 @@ class Solution:
         
         
         
+
+# class Solution:
+#     def isSubsequence(self, s: str, t: str) -> bool:
+#         if len(s) == 0:
+#             return True
+#         idx = 0
+#         for c in t:
+#             if s[idx] == c:
+#                 idx += 1
+#                 if idx == len(s):
+#                     return True
+#         return False
         
         
+        
+        
+        
+        
+        
+        
+    
         
         
         
