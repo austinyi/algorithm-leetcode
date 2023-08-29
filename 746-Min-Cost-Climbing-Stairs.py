21d40f8f169ff8614e0e6cd29bac0d93e3ac80b5
Bongsoo Yi <austinyi@naver.com>
2023-08-28 22:26:51 -0500

    Update 746-Min-Cost-Climbing-Stairs.py

--- 746-Min-Cost-Climbing-Stairs.py
+++ 746-Min-Cost-Climbing-Stairs.py
@@ -1,17 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        a, b = cost[-2], cost[-1]
-        for i in range(len(cost)-3, -1, -1):
-            temp = a
-            a = cost[i] + min(a, b)
-            b = temp     
-        return min(a, b)
+        a, b = cost[0], cost[1]
+        for i in range(2, len(cost)):
+            a, b = b, min(b, a) + cost[i]
+        return min(a,b)
 
-'''
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)
-        for i in range(len(cost) - 3, -1, -1):
-            cost[i] += min(cost[i + 1], cost[i + 2])
-        return min(cost[0], cost[1])
-'''
+
+        # a, b = cost[-2], cost[-1]
+        # for i in range(len(cost)-3, -1, -1):
+        #     temp = a
+        #     a = cost[i] + min(a, b)
+        #     b = temp     
+        # return min(a, b)
