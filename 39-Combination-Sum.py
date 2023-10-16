27a558e3464712076cb3fccc6d54ea8b88442160
Bongsoo Yi <austinyi@naver.com>
2023-10-16 17:25:54 -0500

    Update 39-Combination-Sum.py

--- 39-Combination-Sum.py
+++ 39-Combination-Sum.py
@@ -1,36 +1,14 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
+        ans = []
         def dfs(candidates, target, cur):
-            ans = []
-            if target == 0:
-                return [cur]
-            elif target < 0:
-                return []
+            if target < 0:
+                return
+            elif target == 0:
+                ans.append(cur)
+            else:
+                for i in range(len(candidates)):
+                    dfs(candidates[i:], target - candidates[i], cur + [candidates[i]])
 
-            for i in range(len(candidates)):
-                ans += dfs(candidates[i:], target - candidates[i], cur + [candidates[i]])
-            return ans
-         
-	#ans = []
-        #for i in range(len(candidates)):
-        #    ans += dfs(candidates[i:], target - candidates[i], [candidates[i]])
-
-        ans = dfs(candidates, target, [])
-        
+        dfs(candidates, target, [])
         return ans
-
-    
-'''
-## https://leetcode.com/problems/combination-sum/solutions/632799/Three-Python-easy-solutions-(DPBFSBacktrack)-with-explanation.-Time-beats-~88
-# Dynmaic Programming
-def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-	cache = [[] for _ in range(target + 1)]
-	cache[0] = [[]]
-	for c in candidates:
-		for i in range(target + 1):
-			if i >= c:
-				for temp_ans in cache[i - c]:
-					cache[i].append(temp_ans + [c])
-	return cache[-1]
-'''
