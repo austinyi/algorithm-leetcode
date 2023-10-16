40435583cac217cc9e4273e32fa212876f245660
Bongsoo Yi <austinyi@naver.com>
2023-10-16 17:40:38 -0500

    Update 78-Subsets.py

--- 78-Subsets.py
+++ 78-Subsets.py
@@ -1,13 +1,26 @@
 class Solution:
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+        elif len(nums) == 1:
+            return [[], [nums[0]]]
         ans = []
 
-        def dfs(nums, cur):
-            if nums == []:
-                ans.append(cur)
-            else:
-                dfs(nums[1:], cur)
-                dfs(nums[1:], cur + [nums[0]])
-        
-        dfs(nums, [])
+        for i in self.subsets(nums[1:]):
+            ans.append(i)
+            ans.append([nums[0]] + i)
         return ans
+    
+    # def subsets(self, nums: List[int]) -> List[List[int]]:
+    #     ans = []
+
+    #     def dfs(nums, cur):
+    #         if nums == []:
+    #             ans.append(cur)
+    #         else:
+    #             dfs(nums[1:], cur)
+    #             dfs(nums[1:], cur + [nums[0]])
+        
+    #     dfs(nums, [])
+    #     return ans
