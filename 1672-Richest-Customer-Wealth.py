498ff1ccd7ec306283ecf23288b91db0d35dec3f
Bongsoo Yi <austinyi@naver.com>
2023-11-25 17:08:16 +0900

    Create 1672-Richest-Customer-Wealth.py

---
+++ 1672-Richest-Customer-Wealth.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        ans = 0
+        for account in accounts:
+            ans = max(ans, sum(account))
+        return ans
