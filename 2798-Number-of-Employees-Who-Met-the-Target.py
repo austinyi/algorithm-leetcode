89cdd03ad592cccccb4ae7964a7e6307f4ff22c6
Bongsoo Yi <austinyi@naver.com>
2023-11-25 17:05:02 +0900

    Create 2798-Number-of-Employees-Who-Met-the-Target.py

---
+++ 2798-Number-of-Employees-Who-Met-the-Target.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        ans = 0
+        for h in hours:
+            if h >= target:
+                ans += 1
+
+        return ans
+        
