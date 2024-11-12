96f1023f32b0d1d9c4688e7f8cc1b25bf697fa29
Bongsoo Yi <austinyi@naver.com>
2024-11-12 11:43:36 -0500

    Update 50-Pow(x, n).py

--- 50-Pow(x, n).py
+++ 50-Pow(x, n).py
@@ -1,3 +1,5 @@
+# 1: Binary (Recursive)
+# time: O(logn), space: O(logn)
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
@@ -11,4 +13,23 @@ class Solution:
         else:
             t = self.myPow(x, n // 2)
             return t * t * x
+    
+# 2: Binary (Iterative)
+# time: O(logn), space: O(1)
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n < 0:
+            x = 1 / x
+            n = -n
         
+        ans = 1
+        while n > 0:
+            if n % 2 == 1:
+                ans *= x
+                n -= 1
+            elif n % 2 == 0:
+                x *= x
+                n //= 2
+        return ans
