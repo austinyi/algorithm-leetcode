690b9568283c199f257453a0bcab1d72050989cf
Bongsoo Yi <austinyi@naver.com>
2024-11-11 23:05:40 -0500

    Update 162-Find-Peak-Element.py

--- 162-Find-Peak-Element.py
+++ 162-Find-Peak-Element.py
@@ -1,6 +1,7 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
+        # Find the first index that nums[i] > nums[i+1]. Then naturally nums[i-1] > nums[i]
         while l < r:
             mid = (l+r) // 2
             if nums[mid] > nums[mid+1]:
