ea78a8f3cd92bfb68f2db632e5829935dc47e4a1
Bongsoo Yi <austinyi@naver.com>
2025-01-28 14:02:40 -0500

    Update 9-palindrome-number.py

--- 9-palindrome-number.py
+++ 9-palindrome-number.py
@@ -1,28 +1,36 @@
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         return str(x) == str(x)[::-1]
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
+            return False   
+        elif x % 10 == 0 and x != 0:
             return False
-        if x % 10 == 0 and x != 0:
-            return False
-        
+
         y = 0
-        temp = x
-        while temp > 0:
-            a = temp % 10
-            temp //= 10
-            y = 10*y + a
+        while x > y:
+            y = 10 * y + x % 10
+            x = x // 10
+            print(x,y)
         
-        return y == x
+        return x == y or x == y // 10
 
-    # def isPalindrome(self, x: int) -> bool:
-    #     if x < 0 or (x != 0 and x % 10 == 0):
-    #         return False
 
-    #     reversed_num = 0
-    #     original = x
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+        # if x < 0:
+        #     return False
+        # if x % 10 == 0 and x != 0:
+        #     return False
+        
+        # y = 0
+        # temp = x
+        # while temp > 0:
+        #     a = temp % 10
+        #     temp //= 10
+        #     y = 10*y + a
+        
+        # return y == x
 
-    #     while x > reversed_num:
-    #         reversed_num = reversed_num * 10 + x % 10
-    #         x //= 10
-
-    #     return x == reversed_num or x == reversed_num // 10
