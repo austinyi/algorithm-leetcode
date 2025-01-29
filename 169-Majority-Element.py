519664df80e3cfbe0e7bfc4d90bc23272e644d96
Bongsoo Yi <austinyi@naver.com>
2025-01-29 01:01:30 -0500

    Update 169-Majority-Element.py

--- 169-Majority-Element.py
+++ 169-Majority-Element.py
@@ -1,10 +1,25 @@
+## Time: O(n), Space: O(n)
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = dict()
-        ans, maxCount = nums[0], 0
+        maxCount = 0
         for i in nums:
             count[i] = 1 + count.get(i, 0)
             if count[i] > maxCount:
                 maxCount = count[i]
                 ans = i
         return ans
+
+# Boyer-Moore Voting Algorithm
+## Time: O(n), Space: O(1)
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+
+        return candidate
