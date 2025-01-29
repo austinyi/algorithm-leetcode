08eebb98696ddb2b14e7f07cbd4fe7038266a094
Bongsoo Yi <austinyi@naver.com>
2025-01-29 12:57:00 -0500

    Update 560-Subarray-Sum-Equals-K.py

--- 560-Subarray-Sum-Equals-K.py
+++ 560-Subarray-Sum-Equals-K.py
@@ -1,3 +1,7 @@
+# Brute force: time: O(n^2), space: O(1)
+
+# Using Hashmap
+## time: O(n), space: O(n)
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         ans = 0
@@ -7,7 +11,8 @@ class Solution:
         for i in nums:
             cur += i
             ans += Sn.get(cur - k, 0)
-
             Sn[cur] = 1 + Sn.get(cur, 0)
 
         return ans
+
+
