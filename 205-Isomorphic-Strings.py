171722eada77feecd9fd0eaac924f124f1f9e87c
Bongsoo Yi <austinyi@naver.com>
2025-01-29 22:02:23 -0500

    Update 205-Isomorphic-Strings.py

--- 205-Isomorphic-Strings.py
+++ 205-Isomorphic-Strings.py
@@ -1,12 +1,32 @@
 class Solution(object):
     def isIsomorphic(self, s, t):
         st = {}
-        for i, c in enumerate(s):
-            if c not in st:
-                if t[i] in st.values():
+        for i in range(len(s)):
+            if s[i] in st:
+                if t[i] != st[s[i]]:
                     return False
-                st[c] = t[i]
+            elif t[i] in st.values():
+                return False
             else:
-                if st[c] != t[i]:
-                    return False
+                st[s[i]] = t[i]
+
         return True
+            
+    # def isIsomorphic(self, s, t):
+    #     if len(s) != len(t):
+    #         return False
+
+    #     mapping_s_t = {}
+    #     mapping_t_s = {}
+
+    #     for char_s, char_t in zip(s, t):
+    #         if (char_s in mapping_s_t and mapping_s_t[char_s] != char_t) or \
+    #         (char_t in mapping_t_s and mapping_t_s[char_t] != char_s):
+    #             return False
+
+    #         mapping_s_t[char_s] = char_t
+    #         mapping_t_s[char_t] = char_s
+
+    #     return True
+                
+
