734365f8e6951c4c9ac326232e67fd33d8f75df4
Bongsoo Yi <austinyi@naver.com>
2025-02-01 10:20:46 -0500

    Update 162-Find-Peak-Element.py

--- 162-Find-Peak-Element.py
+++ 162-Find-Peak-Element.py
@@ -1,12 +1,15 @@
+# Iterative Binary Search
+# If the middle element is greater than its right neighbor, 
+# then a peak must exist on the left side (including the middle).
+# Otherwise, a peak must exist on the right side.
+# Time: O(logn), Space: O(1)
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
-        # Find the first index that nums[i] > nums[i+1]. Then naturally nums[i-1] > nums[i]
         while l < r:
-            mid = (l+r) // 2
-            if nums[mid] > nums[mid+1]:
-                r = mid
+            m = (l+r) // 2
+            if nums[m] > nums[m+1]:
+                r = m
             else:
-                l = mid + 1
+                l = m + 1
         return l
-        
