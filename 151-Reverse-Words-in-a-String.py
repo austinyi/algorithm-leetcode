08cee41128c995a3e96a1258c654a0794d1d67a5
Bongsoo Yi <austinyi@naver.com>
2025-02-01 10:27:23 -0500

    Update 151-Reverse-Words-in-a-String.py

--- 151-Reverse-Words-in-a-String.py
+++ 151-Reverse-Words-in-a-String.py
@@ -1,24 +1,24 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        return " ".join(reversed(s.split()))
-
+# Time: O(n)
+# Space Complexity: O(n), Storing the list of words requires additional memory. (s.split())
 class Solution:
     def reverseWords(self, s: str) -> str:
         return ' '.join(s.split()[::-1])
 
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        res = []
-        cur = ""
-        for c in s:
-            if c == " ":
-                if cur == "":
-                    continue
-                else:
-                    res.append(cur)
-                    cur = ""
-            else:
-                cur += c
-        if cur != "":
-            res.append(cur)
-        return ' '.join(res[::-1])
+
+## Implement s.split()
+# class Solution:
+#     def reverseWords(self, s: str) -> str:
+#         res = []
+#         cur = ""
+#         for c in s:
+#             if c == " ":
+#                 if cur == "":
+#                     continue
+#                 else:
+#                     res.append(cur)
+#                     cur = ""
+#             else:
+#                 cur += c
+#         if cur != "":
+#             res.append(cur)
+#         return ' '.join(res[::-1])
