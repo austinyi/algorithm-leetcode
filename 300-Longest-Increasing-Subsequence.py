e8d77399df088cf019a04fd66ede40029a22228d
Bongsoo Yi <austinyi@naver.com>
2025-02-02 01:01:41 -0500

    Update 300-Longest-Increasing-Subsequence.py

--- 300-Longest-Increasing-Subsequence.py
+++ 300-Longest-Increasing-Subsequence.py
@@ -1,10 +1,25 @@
+# DP
+# Time: O(n^2), Space: O(n)
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1]*len(nums)
-        #ans = 1
-        for i in range(1, len(nums)):
+        n = len(nums)
+        dp = [1] * n
+        for i in range(1, n):
             for j in range(i):
-                if nums[j] < nums[i]:
+                if nums[i] > nums[j]:
                     dp[i] = max(dp[j] + 1, dp[i])
-            #ans = max(ans, dp[i])
-        return max(dp) #ans
+        
+        return max(dp)
+
+# Binary Search (very challenging)
+# Time: O(nlogn), Space: O(n)
+class Solution:  
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                idx = bisect_left(sub, x)  # Find the index of the first element >= x
+                sub[idx] = x  # Replace that number with x
+        return len(sub)
